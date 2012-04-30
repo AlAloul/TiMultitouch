03af3332f77dfb45aa3f736e56da3f7c2a89422c
Uri Shaked <uri@salsa4fun.co.il>
2012-05-01 00:10:28 +0300

    Created a better example code for using the module

--- example/app.js
+++ example/app.js
@@ -1,80 +1,66 @@
-// Copyright (c) 2011, Uri Shaked.
-// Based on code originally written by masuidrive.
-// Sample photo copyright (C) Peter Heilmann, under CC-BY 2.0 license
+// Copyright (c) 2012, Uri Shaked.
+// Simple usage example for Titanium Multitouch module.
 
 Ti.UI.setBackgroundColor('#000');
+
+var window = Ti.UI.createWindow({
+	backgroundColor : '#000'
+});
+
+var pointViews = {};
+
+/* --- INTERESTING CODE STARTS HERE --- */
 
 // Step 1: Load the multitouch module
 require("org.urish.titanium.multitouch");
 
-var window = Ti.UI.createWindow({  
-	backgroundColor:'#fff',
-	fullscreen: true
-});
+// Step 2: Enable multitouch for the main window
+window.multitouch = true;
 
-var scale = 1.0;
-
-function calcDistance(x1, y1, x2, y2) {
-	return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-}
-
-var imageView = Ti.UI.createImageView({
-	image: "sample.jpg",
-	transform: Ti.UI.create2DMatrix().scale(scale)
-});
-window.add(imageView);
-
-
-window.addEventListener('singletap', function(ev) {
-	// DON'T REMOVE THIS LISTENER!!
-	// Hack for the multi touch module. If you remove this, some of the touchend events
-	// may not be fired at all!
-});
-
-var start_scale = scale;
-var distance = 0.0;
-var touchstart_point = {x:0, y:0};
-var touchstart_view = {left:0, top:0};
-
-function extractPoints(ev) {
-	var result = [];
-	for (var key in ev.points) {
-		result.push(ev.points[key]);
-	}
-	return result;
-}
-
-window.addEventListener('touchstart', function(ev) {
-	Ti.API.info(ev);
-	var pointArray = extractPoints(ev);
-	if (pointArray.length > 1) {
-		start_scale = scale;
-		distance = calcDistance(pointArray[0].x,pointArray[0].y, pointArray[1].x, pointArray[1].y);
-	} else {
-		touchstart_point.x = ev.x;
-		touchstart_point.y = ev.y;
-		touchstart_view.left = imageView.left || 0;
-		touchstart_view.top = imageView.top || 0;
-	}
-});
-window.addEventListener('touchmove', function(ev) {
-	Ti.API.info(ev);
-	var pointArray = extractPoints(ev);
-	if (pointArray.length > 1) {
-		scale = start_scale * (calcDistance(pointArray[0].x, pointArray[0].y, pointArray[1].x, pointArray[1].y) / distance);
-		imageView.transform = Ti.UI.create2DMatrix().scale(scale);
-	} else {
-		imageView.left = touchstart_view.left + (ev.x - touchstart_point.x);
-		imageView.top = touchstart_view.top + (ev.y - touchstart_point.y); 
+// Step 3: Add event listeners to the view
+window.addEventListener('touchstart', function(event) {
+	Ti.API.info(event);
+	for(var point in event.points) {
+		var radius = 30;
+		var view = Ti.UI.createView({
+			width : radius * 2,
+			height : radius * 2,
+			borderRadius : radius,
+			backgroundColor : '#00a000',
+			center : {
+				x : event.points[point].x,
+				y : event.points[point].y
+			}
+		});
+		pointViews[point] = view;
+		window.add(view);
 	}
 });
 
-window.addEventListener('touchend', function(ev) {
-	Ti.API.info(ev);
+window.addEventListener('touchmove', function(event) {
+	Ti.API.info(event);
+	for(var point in event.points) {
+		pointViews[point].center = {
+			x : event.points[point].x,
+			y : event.points[point].y
+		};
+	}
 });
 
-window.addEventListener('touchcancel', function(ev) {
-	Ti.API.info(ev);
+window.addEventListener('touchend', function(event) {
+	Ti.API.info(event);
+	for(var point in event.points) {
+		window.remove(pointViews[point]);
+		delete pointViews[point];
+	}
+});
+
+window.addEventListener('touchcancel', function(event) {
+	Ti.API.info(event);
+	for(var point in event.points) {
+		window.remove(pointViews[point]);
+		delete pointViews[point];
+	}
 });
 
 window.open();
